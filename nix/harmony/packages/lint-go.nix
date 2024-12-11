a64896f099cc53dc79305b06eae07f7040df048f
Alexis Destrez <krostar@users.noreply.github.com>
2024-12-12 00:06:24 +0100

    [packages/lint-go] Allow script to take arguments

--- nix/harmony/packages/lint-go.nix
+++ nix/harmony/packages/lint-go.nix
@@ -19,14 +19,9 @@ in
   pkgs.writeScriptBin "lint-go" ''
     #!/bin/sh
 
-    if [ "$#" -ne 0 ]; then
-      >&2 echo "This script takes no arguments."
-      exit 1
-    fi
-
     set -o errexit # Exit immediately if a command exits with a non-zero status.
     set -o xtrace  # Print commands and their arguments as they are executed.
 
-    ${golangci-lint}/bin/golangci-lint run --config ${configFile} --verbose
+    ${golangci-lint}/bin/golangci-lint run --config ${configFile} --verbose "$@"
     ${pkgs.govulncheck}/bin/govulncheck -v ./...
   ''
